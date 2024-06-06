0b972f2ea9bedab387cc58e7b1d62d2bb83f7304
Zerihun Moges <zerihunmoges78@gmail.com>
2024-06-06 21:22:09 +0300

    initial setup

---
+++ server/settings/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+def get_secret(secret_id, backup=None):
+    return os.getenv(secret_id, backup)
+
+if get_secret('PIPELINE') == 'production':
+    from .production import *
+else:
+    from .development import *
