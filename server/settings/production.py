b8242b322f842d048bfecfbcf9e4a4a6154a525f
Zerihun Moges <zerihunmoges78@gmail.com>
2024-06-08 05:16:12 +0300

    create foodtrucks api

--- server/settings/production.py
+++ server/settings/production.py
@@ -24,7 +24,7 @@ from . import get_secret
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
