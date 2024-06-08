e60238cf99ac7a7d038fe4344445db0e43d94239
Zerihun Moges <zerihunmoges78@gmail.com>
2024-06-08 10:09:15 +0300

    add cors allowed origins

--- server/settings/local.py
+++ server/settings/local.py
@@ -115,6 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+   
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
